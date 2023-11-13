4feb2dc8b441e02927caa1d722b27f6f820408bb
Daniel Zhang <danielzhang@Daniels-MacBook-Pro.local>
2023-11-13 16:46:03 +1300

    completed revenue report page up to generating the correct reports, and fuctional dropdown

--- public/js/revenueReport.js
+++ public/js/revenueReport.js
@@ -15,37 +15,95 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(data);
 
             let htmlContent = '';
+            htmlContent += ' <div id="revenue-report-name-id-bar">' +
+                              '<div>Student ID</div> <div> Name</div> <div class="w-10">show/hide <i class="fa-solid fa-angles-down"></i></div>' +
+                               '</div>';
 
             for (let studentId in data) {
                 let studentData = data[studentId];
 
-                htmlContent += `<div>
-                <h2>Student ID: ${studentData.id}</h2>
-                <p>First Name: ${studentData.firstname}</p>
-                <p>Surname: ${studentData.surname}</p>`;
+                htmlContent += `<div
+                <div class="generic-row student-row">   
+                    <div> # ${studentData.id} </div> 
+                    <div> ${studentData.firstname} ${studentData.surname} </div>
+                    <div class="w-10">show/hide <i class="fa-solid fa-angles-down"></i></div>
+                </div>
+                <div class="show">`;
 
                 for (let enrolmentNumber in studentData.enrolments) {
                     let enrolmentData = studentData.enrolments[enrolmentNumber];
 
                     htmlContent += `<div>
-                    <h3>Enrolment Number: ${enrolmentNumber}</h3>
-                    <p>Enrolment Date: ${enrolmentData.enrolment_date}</p>`;
+                        <div class="generic-bar" >
+                            <div>Enrolment Number - </div> 
+                            <div>Enrolment Time - </div>
+                            <div class="w-10"></div>
+                        </div>
+                        <div class="generic-row"> 
+                            <div># ${enrolmentNumber}</div> 
+                            <div>${enrolmentData.enrolment_date}</div>
+                            <div class="w-10">show/hide <i class="fa-solid fa-angles-down"></i></div>
+                        </div>`;
 
                     for (let headerNumber in enrolmentData.invoice_headers) {
                         let headerData = enrolmentData.invoice_headers[headerNumber];
 
-                        htmlContent += `<div>
-                        <h4>Header Number: ${headerNumber}</h4>
-                        <p>Amount: ${headerData.amount}</p>
-                        <p>Balance: ${headerData.balance}</p>`;
+                        if(headerData.balance < 0) {
+                            htmlContent += `<div class="show">
+                        <div class="generic-bar-2" >
+                            <div>Header Number - </div> 
+                            <div>Total Due: </div>
+                            <div>Remaining Balance: </div>
+                            <div class="w-10"></div>
+                        </div>
+                        <div class="generic-row-2">
+                            <div># ${headerNumber}</div>
+                            <div> $${headerData.amount}</div>
+                            <div>-$${Math.abs(headerData.balance)} (Overpaid)</div>
+                            <div class="w-10"> show/hide <i class="fa-solid fa-angles-down"></i></div>
+                        </div>
+                        <div class="show">
+                        <div class="generic-bar-2"> 
+                            <div>Invoice Line Number:</div>
+                            <div>Payment Date:</div>
+                            <div>Amount:</div>
+                            <div class="w-10"></div>
+                        </div>`;
+
+                        } else {
+                            htmlContent += `<div class="show">
+                        <div class="generic-bar-2" >
+                            <div>Header Number - </div> 
+                            <div>Total Due: </div>
+                            <div>Remaining Balance: </div>
+                            <div class="w-10"></div>
+                        </div>
+                        <div class="generic-row-2">
+                            <div># ${headerNumber}</div>
+                            <div> $${headerData.amount}</div>
+                            <div>$${headerData.balance}</div>
+                            <div class="w-10">show/hide <i class="fa-solid fa-angles-down"></i></div>
+                        </div>
+                        <div class="show">
+                            <div class="generic-bar-2"> 
+                                <div>Invoice Line Number:</div>
+                                <div>Payment Date:</div>
+                                <div>Amount:</div>
+                                <div class="w-10"></div>
+                            </div>`;}
+
 
                         if (headerData.line_items) {
                             for (let lineItemNumber in headerData.line_items) {
                                 let lineItem = headerData.line_items[lineItemNumber];
                                 htmlContent += `<div>
-                                <p>Invoice Line Number: ${lineItemNumber}</p>
-                                <p>Payment Date: ${lineItem.payment_date}</p>
-                                <p>Amount: ${lineItem.amount}</p>
+            
+                                <div class="generic-row-2">
+                                    <div> # ${lineItemNumber}</div>
+                                    <div> ${lineItem.payment_date}</div>
+                                    <div> $ ${lineItem.amount}</div>
+                                    <a class="w-10"></a>
+                                </div>
                             </div>`;
                             }
                         }
@@ -53,10 +111,10 @@ document.addEventListener('DOMContentLoaded', () => {
                         htmlContent += `</div>`;
                     }
 
-                    htmlContent += `</div>`;
+                    htmlContent += `</div></div>`;
                 }
 
-                htmlContent += `</div>`;
+                htmlContent += `</div></div>`;
             }
 
             // Update the content of the container
@@ -64,5 +122,17 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .catch(error => {
             console.error('Error fetching data:', error);
+        })
+        .then( () => {
+            const showHideButtons = document.querySelectorAll('.generic-row .w-10, .generic-row-2 .w-10');
+            showHideButtons.forEach(button => {
+                button.addEventListener('click', () => {
+                    const sectionToToggle = button.parentElement.nextElementSibling;
+                    if (sectionToToggle) {
+                        sectionToToggle.classList.toggle('hide');
+                        console.log('working');
+                    }
+                });
+            });
         });
 });
